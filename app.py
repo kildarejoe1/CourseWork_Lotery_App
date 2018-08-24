bacf8cc182acb7a9f2da82a177baa8df9ac6495d
iiihenrymorrin <henry.morrin@iii.com>
2018-08-24 11:48:39 +0100

    Adding a create_lottery_numbers function, to calculate random numbers - with random module)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #Udemy Course Work - Lottery App
 
+#Import the random class
+import random
 #Get the Lottery numbers
 def get_players_numbers():
     numbers = str( raw_input("Enter the numbers separated by commas: "))
@@ -9,3 +11,15 @@ def get_players_numbers():
     numbers_in_set={int(x) for x in numbers_in_list}
     #Now return a set from the invoking call
     return numbers_in_set
+
+#Lottery calaculates 6 random nubers
+def create_lottery_numbers():
+    lottery_num=set()
+    for x in range(6):
+        lottery_num.add(random.randint(1,100)) #Prints a random number between 1 and 100
+    return lottery_num
+
+
+
+print(get_players_numbers())
+print(create_lottery_numbers())
